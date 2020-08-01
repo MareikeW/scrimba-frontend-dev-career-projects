a5a1312b514459fd499cd6e98a1e450ad82ca359
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2020-08-01 12:48:16 +0200

    Mobile Layout erstellt

---
+++ search-bar-project/searchPokemon.js
@@ -0,0 +1,6 @@
+document.getElementById("pokemonName").addEventListener("keyup", function(event) {
+    var searchQuery = event.target.value;
+    console.log(searchQuery);
+});
+
+console.log("Hello");
