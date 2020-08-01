90d6421097ce47be89011915e016dafb973c35e3
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2020-08-01 19:19:18 +0200

    Pokemon können jetzt gesucht werden; gesuchtes Pokemon wird angezeigt

--- search-bar-project/searchPokemon.js
+++ search-bar-project/searchPokemon.js
@@ -1,6 +1,13 @@
-document.getElementById("pokemonName").addEventListener("keyup", function(event) {
-    var searchQuery = event.target.value;
-    console.log(searchQuery);
-});
 
-console.log("Hello");
+document.getElementById("pokemonName").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allPokemonInList = document.getElementsByClassName("name");
+    for (let counter = 0; counter < allPokemonInList.length; counter++) {
+        const currentName = allPokemonInList[counter].textContent.toLowerCase();
+        if (currentName.includes(searchQuery)) {
+            allPokemonInList[counter].style.display = 'block';
+        } else {
+            allPokemonInList[counter].style.display = 'none';
+        }
+    }
+});
